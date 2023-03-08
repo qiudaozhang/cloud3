c84c90d4b86c40cac1bbcbb3c1ada69c18a00c15
qiudaozhang <singkingcho@gmail.com>
2023-03-08 15:59:25 +0800

    update test demo

--- py/db/test.py
+++ py/db/test.py
@@ -1,21 +1,17 @@
-from db.Generate import Generate
-import pymysql.cursors
 import pymysql
+import pymysql.cursors
+
+from db.Generate import Generate
 
 config = {
     'host': '127.0.0.1',
-    'port':  3354,  # MySQL默认端口
-    'user':  'root',  # mysql默认用户名
-    'password':  'root',
-    'db':  'cloud3',  # 数据库
+    'port': 3354,  # MySQL默认端口
+    'user': 'root',  # mysql默认用户名
+    'password': 'root',
+    'db': 'cloud3',  # 数据库
     'charset': 'utf8mb4',
     'cursorclass': pymysql.cursors.DictCursor,
 }
-# con = db.custom_conn('127.0.0.1',3354,'root','root','cloud3')
-
-
-# g = Generate(con,'auth','E://generate_code','top.daozhang')
-
-
-g = Generate(config,[],'E://generate_code','top.daozhang','邱道长',jdk_version='8',request_body=False,rest=False,null_validate=True,doc_type='v3',kotlin=False)
+g = Generate(config, [], 'E://generate_code', 'top.daozhang', '邱道长', jdk_version='8', request_body=False, rest=True,
+             null_validate=True, doc_type='v3', kotlin=True)
 g.run()
